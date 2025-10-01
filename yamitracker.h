2c75967cfaa96aacb3188d5351c5a2501016282e
akixurx <duma881luch@gmail.com>
2025-10-01 14:09:41 +0300

    Update yamitracker.h

--- yamitracker.h
+++ yamitracker.h
@@ -1,21 +1,54 @@
-#ifndef YAMITRACKER_H
-#define YAMITRACKER_H
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPushButton>
+#include <QLabel>
+#include <QSlider>
+#include <QComboBox>
+#include <QGridLayout>
+#include <QWidget>
+#include <QGroupBox>
+#include <QMap>
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class Yamitracker; }
-QT_END_NAMESPACE
-
-class Yamitracker : public QMainWindow
+class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    Yamitracker(QWidget *parent = nullptr);
-    ~Yamitracker();
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void onNotePressed(int note);
+    void onNoteReleased(int note);
+    void onVolumeChanged(int value);
+    void onInstrumentChanged(int index);
+    void onOctaveChanged(int value);
 
 private:
-    Ui::Yamitracker *ui;
+    void setupUI();
+    void createPianoKeys();
+    void createControlPanel();
+    void createDisplay();
+    
+    // Piano keys
+    QWidget *pianoWidget;
+    QMap<int, QPushButton*> whiteKeys;
+    QMap<int, QPushButton*> blackKeys;
+    
+    // Controls
+    QSlider *volumeSlider;
+    QSlider *octaveSlider;
+    QComboBox *instrumentCombo;
+    QLabel *volumeLabel;
+    QLabel *octaveLabel;
+    QLabel *displayLabel;
+    
+    // Status
+    QLabel *statusLabel;
+    
+    // Style
+    void applyModernStyle();
 };
-#endif // YAMITRACKER_H
+#endif // MAINWINDOW_H
